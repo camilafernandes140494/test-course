8df45aa6964171bdb612f0b84cc2a66dc5509d22
Camila <camila.silva@nappsolutions.com>
2024-07-19 15:08:01 -0300

    feat: add file Dashboard

---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+export function Dashboard() {
+    return (
+        <div className="h-screen flex flex-col items-center justify-center gap-2">
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
