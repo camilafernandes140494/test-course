656dbafe61750d0b45139982d90302704096babd
Camila <camila.silva@nappsolutions.com>
2024-07-18 21:38:54 -0300

    feat: create project and add test and tailwind dependencies

---
+++ src/App.spec.tsx
@@ -0,0 +1,7 @@
+describe("Testa o componente", () => {
+    test("1 + 1 deve ser 2", () => {
+        const sum = 1 + 1;
+        expect(sum).toBe(2);
+    });
+    test("Deve haver um titulo na página", () => { });
+});
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <p className="text-zinc-300 text-lg">
+        Hello Word
+      </p>
+    </div>
+  )
+}
+
+
