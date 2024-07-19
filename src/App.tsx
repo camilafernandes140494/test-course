1dc9586a542b83b916ee2ffd0e434e28e4ed01fc
Camila <camila.silva@nappsolutions.com>
2024-07-18 22:02:42 -0300

    fix: add autoprefixer, postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
-import './App.css'
-
 export function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <p className="text-zinc-300 text-lg">
-        Hello Word
+        Hello Word!!
       </p>
     </div>
   )
