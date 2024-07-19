3ab12fc4e6e5d7a613b0cda5f35770fa17840d76
Camila <camila.silva@nappsolutions.com>
2024-07-19 15:38:48 -0300

    feat: add new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./Login/Login";
+import { Dashboard } from "./Dashboard/Dashboard";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 export function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
-      <p className="text-zinc-300 text-lg">
-        Hello Word!!
-      </p>
-    </div>
-  )
+    <RouterProvider router={router} />
+  );
 }
-
-
